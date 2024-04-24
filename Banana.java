98fc64062e8879168995412227119cc814d6c209
herman888 <341140944@gapps.yrdsb.ca>
2024-04-24 16:56:05 -0400

    Make Banana Reset

--- Banana.java
+++ Banana.java
@@ -6,5 +6,19 @@ public class Banana extends Actor
     public void act()
     {
         move(-3); 
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0){
+            setLocation(600,100);
+        }
+        else {
+            setLocation(600,100);
+        }
     }
 }
