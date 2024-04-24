7698a9f3a178ce7f4ede16b335c92e3d7cc8c595
herman888 <341140944@gapps.yrdsb.ca>
2024-04-24 17:06:31 -0400

    Make Banana Dissapear

--- Banana.java
+++ Banana.java
@@ -10,6 +10,11 @@ public class Banana extends Actor
         if(getX() <= 0) {
             resetBanana();
         }
+        
+        if(isTouching(Hero.class)){
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);        }
     }
     
     public void resetBanana(){
