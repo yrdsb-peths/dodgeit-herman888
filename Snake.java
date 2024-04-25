c035d540b286951d5a976c1d4c8b9e5af071e6e9
herman888 <341140944@gapps.yrdsb.ca>
2024-04-25 19:33:23 -0400

    Movement based off keyboard for snake

--- Snake.java
+++ Snake.java
@@ -1,19 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Snake here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Snake extends Actor
 {
-    /**
-     * Act - do whatever the Snake wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public void act()
     {
-        // Add your action code here.
+        move(3);
+        if(Greenfoot.isKeyDown("right"))
+            setRotation(0);
+        if(Greenfoot.isKeyDown("left"))
+            setRotation(180);
+        if(Greenfoot.isKeyDown("up"))
+            setRotation(270);
+        if(Greenfoot.isKeyDown("down"))
+            setRotation(90);
+            
+        
     }
 }
